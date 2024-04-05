669a76aa2b46bb90c8855f90bc953c1ab7425983
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-05 23:54:18 +0500

    Add files via upload

---
+++ 12_greeting/12.Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["zafar", "ali", "rehman", "adnan"];
+const message = `welcome to typscript class: `;
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_greeting/12.Names.ts
@@ -0,0 +1,7 @@
+let names:string[]=["zafar","ali","rehman","adnan"];
+const message:string=`welcome to typscript class: `
+
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}

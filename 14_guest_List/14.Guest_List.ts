ea327a5e4d2cf6f8698a0d2cc0b7d95503426cd2
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-06 00:02:27 +0500

    Add files via upload

---
+++ 14_guest_List/14.Guest_List.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_List:string[]=['Nawaz sharif','Imran khan','Asif ali zardari'];
+
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!')
+
+}

f5a14086b08eac616446c825dd35a79146d4ba47
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-05 23:57:45 +0500

    Add files via upload

---
+++ 13_your_Own_Array/13.your_own_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transpotation = ['motorcycle', 'car', 'train', 'cycle'];
+for (let i = 0; i < transpotation.length; i++) {
+    console.log('i would like to own a ' + transpotation[i]);
+}
---
+++ 13_your_Own_Array/13.your_own_array.ts
@@ -0,0 +1,6 @@
+
+let transpotation :string [] = ['motorcycle','car','train','cycle'];
+
+for(let i=0; i<transpotation.length;i++){
+     console.log('i would like to own a ' + transpotation[i])
+}

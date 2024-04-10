fee7bf3ebcad7760ef97ff06bbcee34b1472e9c1
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:39:45 +0500

    Add files via upload

---
+++ 35_animals/35.animals.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["dog", "cat", "rabbit"];
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+console.log(`Any of these animal would make a great pet !.`);

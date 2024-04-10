9e58b9bc680d3ab52306b2b75f5625d08b955516
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:45:54 +0500

    Add files via upload

---
+++ 39_city_Names/39.city_Names.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_Country(city, country) {
+    return `${city},${country}`;
+}
+console.log(city_Country("Karachi", "Pakistan"));
+console.log(city_Country("Tokyo", "Japan"));
+console.log(city_Country("Paris", "France"));

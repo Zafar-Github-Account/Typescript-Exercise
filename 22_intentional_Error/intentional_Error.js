999736dac031c1a5adc9b7abd4c47ff8b9698734
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:08:55 +0500

    Add files via upload

---
+++ 22_intentional_Error/intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+// if you haven't received an array index error in one of your programs yet, try to make one happen.
+// creating array
+let new_Array = ['zafar', 'ali', 'rehman'];
+// acessing array by index
+console.log(new_Array[5]);
+// make sure you correct the error before closing the program.
+// correct the error
+console.log(new_Array);

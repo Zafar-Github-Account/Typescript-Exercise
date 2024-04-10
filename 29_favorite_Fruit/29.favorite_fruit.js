f516ecc97e246cb19963f328113f2d858b293b09
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:24:28 +0500

    Add files via upload

---
+++ 29_favorite_Fruit/29.favorite_fruit.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_Fruit = ['apple', 'banana', 'mango'];
+if (favorite_Fruit.includes('apple')) {
+    console.log("i really like apple");
+}
+if (favorite_Fruit.includes('banana')) {
+    console.log("i really like banana");
+}
+if (favorite_Fruit.includes('mango')) {
+    console.log("i really like mango");
+}
+if (favorite_Fruit.includes('strawberry')) {
+    console.log("i really like strawberry");
+}
+if (favorite_Fruit.includes('orange')) {
+    console.log("i really like orange");
+}

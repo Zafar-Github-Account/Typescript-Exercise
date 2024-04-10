49807137920f98d7acff7a864c7082897bc968e1
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:17:07 +0500

    Add files via upload

---
+++ 26_alien_Colors_2/26.Alien_color_2.js
@@ -0,0 +1,18 @@
+"use strict";
+// create a variable called alien_Color_2:
+let alien_Color = "green";
+// write an if-else chain
+// if the alien color is green 
+if (alien_Color === "green") {
+    console.log("player just earned 5 points shooting the alien ");
+}
+else {
+    console.log("player earned 10 piont.");
+}
+alien_Color = "yellow";
+if (alien_Color === "green") {
+    console.log("player just earned 5 points shooting the alien ");
+}
+else {
+    console.log("player earned 10 piont.");
+}

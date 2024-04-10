02ca4584c4237851594ca30763f2eef78883a176
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:38:02 +0500

    Add files via upload

---
+++ 34_pizzas/34.pizzas.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas = ["pepperoni", "cuba", "BBQ chiken"];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`i really like pizza`);
---
+++ 34_pizzas/34.pizzas.ts
@@ -0,0 +1,7 @@
+let pizzas : string[] =["pepperoni","cuba","BBQ chiken"];
+
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`i really like pizza`);
+

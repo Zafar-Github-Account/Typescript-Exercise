e64ddfaeca6f36f7ec02daaf3edf95250d809f57
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:41:52 +0500

    Add files via upload

---
+++ 36_T-Shirt/36.t_shirt.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, text) {
+    console.log(`creating a ${size} shirt with the message:${text}`);
+}
+make_shirt("large", "hello world");
+make_shirt("medium", "hello world");
+make_shirt("small", "hello world");

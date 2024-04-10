0c00c77bcf682c85b83aa3039cb043bf312efbc2
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:31:04 +0500

    Add files via upload

---
+++ 31_no_Users/31.No_Users.js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames = ["admin", "jumaid", "zeeshan", "kamran", "daniyal"];
+if (userNames.length === 0) {
+    console.log("we need find some users!");
+}
+else {
+    userNames = [];
+    console.log("all user name have been removed." + userNames.length);
+}
---
+++ 31_no_Users/31.No_Users.ts
@@ -0,0 +1,8 @@
+let userNames:string[]=["admin","jumaid","zeeshan","kamran","daniyal"]
+if(userNames.length === 0){
+    console.log("we need find some users!");
+}
+else{
+    userNames=[];
+    console.log("all user name have been removed." + userNames.length)
+}

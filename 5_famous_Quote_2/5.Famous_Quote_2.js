b20cd937ee2c28b40d52097ce99e1cf5027807ea
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 23:36:16 +0500

    Delete 5_famous_Quote_2 directory

--- 5_famous_Quote_2/5.Famous_Quote_2.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Albert Einstein";
-var message = '"A person who never a mistake never tried anything new."';
-console.log("".concat(famous_person, " once said, ").concat(message));
--- 5_famous_Quote_2/5.Famous_Quote_2.ts
+++
@@ -1,4 +0,0 @@
-let famous_person:string ="Albert Einstein";
-let message:string = '"A person who never a mistake never tried anything new."';
-console.log(`${famous_person} once said, ${message}`);
-

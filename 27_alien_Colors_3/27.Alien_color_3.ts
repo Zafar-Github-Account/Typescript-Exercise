a7d679b62f6e5f7d69bbc086e36d6161b2d00a9d
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:18:59 +0500

    Add files via upload

---
+++ 27_alien_Colors_3/27.Alien_color_3.ts
@@ -0,0 +1,58 @@
+
+// create a variable called alien_Color_3:
+let alien_Color:string="green"; 
+
+// write an if-else chain
+// if the alien color is green 
+
+if (alien_Color==="green"){
+    console.log("version 1:that the earned 5 points.");
+}
+else if(alien_Color==="yellow"){
+    console.log("that the earned 10 points.");
+}
+else if(alien_Color==="red"){
+    console.log("that the earned 15 points.");
+}
+
+else{
+    console.log("please select right color");
+}
+
+// version 2
+alien_Color= "yellow";
+
+if (alien_Color==="green"){
+    console.log("that the earned 5 points.");
+}
+else if(alien_Color==="yellow"){
+    console.log("version 2:that the earned 10 points.");
+}
+else if(alien_Color==="red"){
+    console.log("that the earned 15 points.");
+}
+
+else{
+    console.log("please select right color");
+}
+
+// version 3
+alien_Color="red";
+
+if (alien_Color==="green"){
+    console.log("that the earned 5 points.");
+}
+else if(alien_Color==="yellow"){
+    console.log("that the earned 10 points.");
+}
+else if(alien_Color==="red"){
+    console.log("version 3:that the earned 15 points.");
+}
+
+else{
+    console.log("please select right color");
+}
+
+
+
+

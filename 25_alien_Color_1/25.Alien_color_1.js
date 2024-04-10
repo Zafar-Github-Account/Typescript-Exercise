e463521c2d4ef64f089b4e8247e3e6daddb8ae82
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:15:08 +0500

    Add files via upload

---
+++ 25_alien_Color_1/25.Alien_color_1.js
@@ -0,0 +1,16 @@
+"use strict";
+// create a variable called alien_Color:
+let alien_Color = "green";
+// write an if statement to test wehether the alien color is green .if it is 
+// print a message that the player just earned 5 points . 
+if (alien_Color === "green") {
+    console.log("player Just Eaened 5 Point !");
+}
+// write one version of this program that passes the if test and another that 
+// fails.
+alien_Color = "yellow";
+// (the version that fails will have no output .)
+if (alien_Color === "green") {
+    console.log("player just earned 5 piont !");
+}
+``;

675061f65ddcc15572ced42f604b890c4dc538e8
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:48:32 +0500

    Add files via upload

---
+++ 41_magicians/41.magicians.js
@@ -0,0 +1,8 @@
+"use strict";
+let magician = ['Harry Potter', "Hermione Granger", "Ron Weasley", "Albus Dumbledore"];
+function show_Magicians(magician) {
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+show_Magicians(magician);
---
+++ 41_magicians/41.magicians.ts
@@ -0,0 +1,8 @@
+let magician:string[]=['Harry Potter',"Hermione Granger","Ron Weasley","Albus Dumbledore"];
+
+function show_Magicians(magician:string[]){
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+show_Magicians(magician);

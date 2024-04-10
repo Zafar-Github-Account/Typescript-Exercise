031cff893df9de3d0dc80fb9c1c4b83f263ce099
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:01:24 +0500

    Add files via upload

---
+++ 17_shrinking_Guest_List/17.Guest_List.js
@@ -0,0 +1,29 @@
+"use strict";
+let Guest_List = ['Imran khan', 'Nawaz sharif', 'Asif ali zardari'];
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n')
+// }
+let absent_Guest = 'Imran Khan';
+let new_Guest = 'kamran Tessori';
+Guest_List[0] = new_Guest;
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n')
+// }
+console.log(`Mr.${absent_Guest} is not coming to the party. `);
+console.log('good News! We Find Big Table So We Are Inviting 3 More Guest,');
+Guest_List.unshift('sir zia khan');
+Guest_List.splice(2, 0, 'maryam Nawaz');
+Guest_List.push('bilawal Bhutto Zardari');
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n');
+}
+console.log('\nSorry we can not arrang big table,Only two peoples will be invited.');
+while (Guest_List.length > 2) {
+    let remove_Guest = Guest_List.pop();
+    console.log(`sorry Mr.${remove_Guest}, you are not invited for dinner. `);
+}
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr ' + Guest_List[i] + ',\n\nyou are still invited.\n\nThank you!\n\n');
+}
+Guest_List.splice(0, 2);
+console.log(Guest_List);

6c51604fffa799f1140b918d0ba37ac1ed4f22a9
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:51:39 +0500

    Add files via upload

---
+++ 43_unchanged_Magicians/42.great_Magicians.js
@@ -0,0 +1,21 @@
+"use strict";
+let magician = ['Harry Potter', "Hermione Granger", "Ron Weasley", "Albus Dumbledore"];
+function copy_Array(arr) {
+    return [...arr];
+}
+function make_Great(magician_Array) {
+    for (let i = 0; i < magician_Array.length; i++) {
+        magician_Array[i] = "The Great " + magician_Array[i];
+    }
+}
+function show_Magicians(magician) {
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+const copy_Magician_Array = copy_Array(magician);
+make_Great(copy_Magician_Array);
+console.log("\n\nThis is my orignal array: ");
+show_Magicians(magician);
+console.log("\n\nThis is my modified copy of the array: ");
+show_Magicians(copy_Magician_Array);

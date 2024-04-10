ec4157f9a4035e40007ef77bf5dc7abb2a1c0290
Zafar-Github-Account <166161455+Zafar-Github-Account@users.noreply.github.com>
2024-04-10 17:10:32 +0500

    Add files via upload

---
+++ 23_conditional_Tests/23.conditional_Test.ts
@@ -0,0 +1,48 @@
+// write a series of conditional tests.
+
+// creating car variable
+let car : string = 'subaru';
+
+// test no 1
+console.log("is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+
+// test no 2
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+
+// test no 3
+console.log("is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+
+// test no 4
+console.log("is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+
+// test no 5
+console.log("is car.Upper_case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// test no 6
+console.log("is car== 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+
+// test no 7
+console.log("is car=== 'SUBARU'? I predict false.");
+console.log(car === 'SUBARU');
+
+// test no 8
+console.log("is car=== 'train'? I predict false.");
+console.log(car === 'train');
+
+// test no 9
+console.log("is car=== 'bus'? I predict false.");
+console.log(car === 'bus');
+
+// test no 10
+console.log("is car=== 'cycle'? I predict false.");
+console.log(car === 'cycle');
+
+
+
